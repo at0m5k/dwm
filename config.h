bf3be08fe252a2facf481b4bd363111a51dee527
at0m5k <at0m5k@outlook.com>
2013-09-09 11:36:21 -0400

    +singularborders, -uselessgaps, -single_window_no_border

--- config.h
+++ config.h
@@ -2,7 +2,7 @@
 #include <X11/XF86keysym.h>
 
 /* appearance */
-static const char font[]            = "Menlo for Powerline:size=9";
+static const char font[] = "Ricty:size=10";
 
 #define NUMCOLORS 14
 static const char colors[NUMCOLORS][ColLast][21] = {
@@ -24,7 +24,6 @@ static const char colors[NUMCOLORS][ColLast][21] = {
 };
 
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int gappx     = 2;        /* gap pixel between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const Bool showbar           = True;     /* False means no bar */
 static const Bool topbar            = True;     /* False means bottom bar */
@@ -39,33 +38,33 @@ static const char plclosedsym[]	 = "";	    /* powerline: left closed glyph */
 static const char plsystraysym[] = "";	    /* powerline: right closed glyph */
 
 /* layout(s) */
-static const float mfact      = 0.50;	/* factor of master area size [0.05..0.95] */
-static const int nmaster      = 1;	/* number of clients in master area */
-static const Bool resizehints = False;	/* True means respect size hints in tiled resizals */
+static const float mfact      = 0.50; /* factor of master area size [0.05..0.95] */
+static const int nmaster      = 1;    /* number of clients in master area */
+static const Bool resizehints = False;/* True means respect size hints in tiled resizals */
 
 static const Layout layouts[] = {
-	/* symbol   gaps    arrange function */
-	{ "[]=",    True,   tile },    /* first entry is default */
-	{ "[M]",    False,  monocle },
-	{ "><>",    False,  NULL },    /* no layout function means floating behavior */
+	/* symbol     arrange function */
+	{ "[]=",      tile },    /* first entry is default */
+	{ "[=]",      bstack },
+	{ "[ ]",      monocle },
+	{ "><>",      NULL },    /* no layout function means floating behavior */
 };
 
 /* tagging */
-static Tag tags[] = {
-	/* name     layout      mfact   nmaster */
-	{ "Home",   &layouts[0], -1,    -1 },
-	{ "Term",   &layouts[0], -1,    -1 },
-	{ "code",   &layouts[1], -1,    -1 },
-	{ "Web",    &layouts[1], -1,    -1 },
-	{ "Misc",   &layouts[2], -1,    -1 },
+static const Tag tags[] = {
+	/* name	    layout	 mfact	nmaster */
+	{ "一 home",   &layouts[0], -1,	-1 },
+	{ "二 term",   &layouts[0], -1,	-1 },
+	{ "三 code",   &layouts[0], -1,	-1 },
+	{ "四 web",    &layouts[1], -1,	-1 },
+	{ "五 misc",   &layouts[3], -1,	-1 },
 };
 
 static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Chromium", NULL,       NULL,       1 << 3,       False,       -1 },
-	{ "Skype",    NULL,       NULL,       0,	    True,	 -1 },
+	{ "Firefox",  NULL,       NULL,       1 << 3,       False,       -1 },
+	{ "Skype",    NULL,	  NULL,	      0,	    True,	 -1 },
 };
-
 /* key definitions */
 #define MODKEY Mod1Mask
 #define WINKEY Mod4Mask
@@ -80,22 +79,21 @@ static const Rule rules[] = {
 
 /* commands */
 static const char *dmenu[]	= { "dmenu_run", "-fn", font, "-nb", colors[0][ColBG], "-nf", colors[0][ColFG], "-sb", colors[1][ColBG], "-sf", colors[1][ColFG], NULL };
-static const char *scratchpad[] = { "urxvtc", "-title", scratchpadname, "-geometry", "70x20", NULL };
-static const char *term[]	= { "urxvtc", NULL };
+static const char *pad[]	= { "termite", "--title", scratchpadname, "--geometry", "640x240", NULL };
+static const char *term[]	= { "termite", NULL };
 static const char *volup[]	= { "amixer", "-q", "set", "Master", "5%+", NULL };
 static const char *voldown[]	= { "amixer", "-q", "set", "Master", "5%-", NULL };
 static const char *volmute[]	= { "amixer", "-q", "set", "Master", "toggle", NULL };
-static const char *lock[]	= { "slock", NULL };
 static const char *suspend[]	= { "systemctl", "suspend", NULL };
 static const char *play[]	= { "ncmpcpp", "toggle", NULL };
 static const char *next[]	= { "ncmpcpp", "next", NULL };
 static const char *prev[]	= { "ncmpcpp", "prev", NULL };
 
 static Key keys[] = {
-	/* modifier                 key        function        argument */
-	{ MODKEY,		XK_p,			    spawn,          {.v = dmenu } },
+	/* modifier                     key        function        argument */
+	{ MODKEY,               XK_p,			    spawn,          {.v = dmenu } },
 	{ MODKEY|ShiftMask,	XK_Return,		    spawn,          {.v = term } },
-	{ 0,			XK_F12,			    togglescratch,  {.v = scratchpad } },
+	{ MODKEY,		XK_grave,		    togglescratch,  {.v = pad } },
 	{ 0,			XF86XK_AudioRaiseVolume,    spawn,	    {.v = volup } },
 	{ 0,			XF86XK_AudioLowerVolume,    spawn,	    {.v = voldown } },
 	{ 0,			XF86XK_AudioMute,	    spawn,	    {.v = volmute } },
@@ -103,34 +101,34 @@ static Key keys[] = {
 	{ 0,			XF86XK_AudioNext,	    spawn,	    {.v = next } },
 	{ 0,			XF86XK_AudioPrev,	    spawn,	    {.v = prev } },
 	{ WINKEY,		XK_Escape,		    spawn,	    {.v = suspend } },
-	{ WINKEY,		XK_F1,			    spawn,	    {.v = lock } },
-	{ MODKEY,		XK_b,			    togglebar,      {0} },
-	{ MODKEY,		XK_j,			    focusstack,     {.i = +1 } },
-	{ MODKEY,		XK_k,			    focusstack,     {.i = -1 } },
-	{ MODKEY,		XK_i,			    incnmaster,     {.i = +1 } },
-	{ MODKEY,		XK_d,			    incnmaster,     {.i = -1 } },
-	{ MODKEY,		XK_h,			    setmfact,       {.f = -0.05} },
-	{ MODKEY,		XK_l,			    setmfact,       {.f = +0.05} },
-	{ MODKEY,		XK_Return,		    zoom,           {0} },
-	{ MODKEY,		XK_Tab,			    view,           {0} },
-	{ MODKEY|ShiftMask,	XK_c,			    killclient,     {0} },
-	{ MODKEY,		XK_t,			    setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,		XK_f,			    setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,		XK_m,			    setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,		XK_space,		    setlayout,      {0} },
-	{ MODKEY|ShiftMask,	XK_space,		    togglefloating, {0} },
-	{ MODKEY,		XK_0,			    view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,	XK_0,			    tag,            {.ui = ~0 } },
-	{ MODKEY,		XK_comma,		    focusmon,       {.i = -1 } },
-	{ MODKEY,		XK_period,		    focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,	XK_comma,		    tagmon,         {.i = -1 } },
+	{ MODKEY|ControlMask,   XK_b,			    togglebar,      {0} },
+	{ MODKEY,               XK_j,			    focusstack,     {.i = +1 } },
+	{ MODKEY,               XK_k,			    focusstack,     {.i = -1 } },
+	{ MODKEY,               XK_i,			    incnmaster,     {.i = +1 } },
+	{ MODKEY,               XK_d,			    incnmaster,     {.i = -1 } },
+	{ MODKEY,               XK_h,			    setmfact,       {.f = -0.05} },
+	{ MODKEY,               XK_l,			    setmfact,       {.f = +0.05} },
+	{ MODKEY,               XK_Return,		    zoom,           {0} },
+	{ MODKEY,               XK_Tab,			    view,           {0} },
+	{ MODKEY|ShiftMask,     XK_c,			    killclient,     {0} },
+	{ MODKEY,               XK_t,			    setlayout,      {.v = &layouts[0] } },
+	{ MODKEY,               XK_b,			    setlayout,      {.v = &layouts[1] } },
+	{ MODKEY,               XK_m,			    setlayout,      {.v = &layouts[2] } },
+	{ MODKEY,		XK_f,			    setlayout,	    {.v = &layouts[3] } },
+	{ MODKEY,               XK_space,		    setlayout,      {0} },
+	{ MODKEY|ShiftMask,     XK_space,		    togglefloating, {0} },
+	{ MODKEY,               XK_0,			    view,           {.ui = ~0 } },
+	{ MODKEY|ShiftMask,     XK_0,			    tag,            {.ui = ~0 } },
+	{ MODKEY,               XK_comma,		    focusmon,       {.i = -1 } },
+	{ MODKEY,               XK_period,		    focusmon,       {.i = +1 } },
+	{ MODKEY|ShiftMask,     XK_comma,		    tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,	XK_period,		    tagmon,         {.i = +1 } },
-	TAGKEYS(		XK_1,			    0)
-	TAGKEYS(		XK_2,			    1)
-	TAGKEYS(		XK_3,			    2)
-	TAGKEYS(		XK_4,			    3)
-	TAGKEYS(		XK_5,			    4)
-	{ MODKEY|ShiftMask,	XK_q,			    quit,	    {0} },
+	TAGKEYS(                XK_1,			    0)
+	TAGKEYS(                XK_2,			    1)
+	TAGKEYS(                XK_3,			    2)
+	TAGKEYS(                XK_4,			    3)
+	TAGKEYS(                XK_5,			    4)
+	{ MODKEY|ShiftMask,     XK_q,			    quit,           {0} },
 };
 
 /* button definitions */
@@ -149,3 +147,4 @@ static Button buttons[] = {
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
+
